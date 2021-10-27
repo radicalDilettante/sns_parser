4ad54de72bd3d5a85e3661461d36197c98c44139
WAYNE CHOI <wonjun92@gmail.com>
2021-10-28 06:44:28 +1300

    connect mongoDB

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 import express from "express";
 import scraperRouter from "./routes/entire_instagram.js";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
-dotenv.config();
+const server = async () => {
+  try {
+    dotenv.config();
+    await mongoose.connect(process.env.DB_URI);
+    console.log("DB connected");
 
-const app = express();
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
+    const app = express();
+    app.get("/", (req, res) => {
+      res.send("hello world");
+    });
 
-app.use("/scraper", scraperRouter);
+    app.use("/scraper", scraperRouter);
 
-app.listen(3000, () => {
-  console.log("Server listening on port 3000");
-});
+    app.listen(3000, () => {
+      console.log("Server listening on port 3000");
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+server();
