b5112db90190147bc505f5cea4bed0f968e0bb65
WAYNE CHOI <wonjun92@gmail.com>
2021-11-13 08:30:15 +1300

    add checkDB logic

--- routes/instagram.js
+++ routes/instagram.js
@@ -1,27 +1,28 @@
 import express from "express";
-import { getFrontList, getDetail } from "../lib/instagram.js";
+import {
+  getFrontList,
+  getFullList,
+  updateAll,
+  checkDb,
+} from "../lib/instagram.js";
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
+  res.sendStatus(200);
   const artistId = req.query.id;
   //bts.bighitofficial
   const url = `https://www.instagram.com/${artistId}`;
   const frontList = await getFrontList(url);
-  const listWithTime = await getDetail(frontList);
-  console.log(listWithTime);
-  res.sendStatus(200);
+  await checkDb("bts", frontList);
 });
 
 router.get("/full", async (req, res) => {
+  res.sendStatus(200);
   const artistId = req.query.id;
-  //bts.bighitofficial
   const url = `https://www.instagram.com/${artistId}`;
   const fullList = await getFullList(url);
-  fullList.forEach((post) => {
-    console.log(post);
-  });
-  res.sendStatus(200);
+  await updateAll("bts", fullList);
 });
 
 export default router;
