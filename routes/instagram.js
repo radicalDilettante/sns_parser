1d5824e432230e03de64bcb54d08306056040aec
WAYNE CHOI <wonjun92@gmail.com>
2021-11-15 12:12:26 +1300

    refactor instagram scraper

--- routes/instagram.js
+++ routes/instagram.js
@@ -5,24 +5,36 @@ import {
   updateAll,
   checkDb,
 } from "../lib/instagram.js";
+import { instagramIds } from "../lib/data.js";
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  res.sendStatus(200);
-  const artistId = req.query.id;
-  //bts.bighitofficial
+  const artist = req.query.artist;
+  const artistId = instagramIds[artist];
+  if (artistId) {
+    res.status(200).json({ message: `got request for ${artist}` });
+  } else {
+    res.status(400).json({ message: "wrong artist name" });
+  }
   const url = `https://www.instagram.com/${artistId}`;
   const frontList = await getFrontList(url);
-  await checkDb("bts", frontList);
+  await checkDb(artist, frontList);
+  console.log("process done");
 });
 
 router.get("/full", async (req, res) => {
-  res.sendStatus(200);
-  const artistId = req.query.id;
+  const artist = req.query.artist;
+  const artistId = instagramIds[artist];
+  if (artistId) {
+    res.status(200).json({ message: `got request for ${artist}` });
+  } else {
+    res.status(400).json({ message: "wrong artist name" });
+  }
   const url = `https://www.instagram.com/${artistId}`;
   const fullList = await getFullList(url);
-  await updateAll("bts", fullList);
+  await updateAll(artist, fullList);
+  console.log("process done");
 });
 
 export default router;
